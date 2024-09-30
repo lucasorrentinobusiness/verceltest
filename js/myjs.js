116cbc4c50d28536c72a3b372353091fcae7630f
lucasorrentinobusiness <153774130+lucasorrentinobusiness@users.noreply.github.com>
2024-09-30 22:27:14 +0200

    Create myjs.js

---
+++ js/myjs.js
@@ -0,0 +1,12 @@
+<script src="https://cdn.signalfx.com/o11y-gdi-rum/latest/splunk-otel-web.js" crossorigin="anonymous"></script>
+<script>
+    SplunkRum.init({
+        realm: "eu1",
+        rumAccessToken: "UguMGKzi8yrquTbVeoGd9Q",
+        applicationName: "verceltest",
+        deploymentEnvironment: "test"
+    });
+</script>
+
+Copy to ClipboardShow less 
+The code was c
