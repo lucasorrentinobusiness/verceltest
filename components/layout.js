b573d85e35e387f7bb376358b9728cf8f42401ef
lucasorrentinobusiness <153774130+lucasorrentinobusiness@users.noreply.github.com>
2024-09-30 22:52:49 +0200

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
-import Script from 'next/script';
 
 const name = 'Elkjop next ECOM';
 
