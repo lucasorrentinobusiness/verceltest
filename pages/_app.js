61864841782b30f247435527989184cf44b68bc0
lucasorrentinobusiness <153774130+lucasorrentinobusiness@users.noreply.github.com>
2024-09-30 22:51:58 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,16 @@
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+   <> 
+      <Component {...pageProps} />
+      <Script
+  	        src="https://cdn.signalfx.com/o11y-gdi-rum/latest/splunk-otel-web.js"
+  	        strategy="lazyOnload"
+  	        onLoad={() =>
+  	          console.log(`script loaded correctly, window.FB has been populated`)
+  	        }
+  	   />
+   </>
+  )
 }
